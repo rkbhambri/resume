36b6759b9224b20bbd448e39a76f4bb080e94685
ram <ramkrishan@kontikilabs.com>
2019-05-03 11:00:49 +0530

    Updates

---
+++ precache-manifest.8051c87e74272cf02a6fa37b2742eae2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c709e1a6e77e6a43c27150195c5158",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "44ee6832bdd8092bab3f",
+    "url": "/resume/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a6b0418ec2dbcc05b5e6",
+    "url": "/resume/static/css/main.0cee59ec.chunk.css"
+  },
+  {
+    "revision": "44ee6832bdd8092bab3f",
+    "url": "/resume/static/js/2.f21a971d.chunk.js"
+  },
+  {
+    "revision": "a6b0418ec2dbcc05b5e6",
+    "url": "/resume/static/js/main.c24952f2.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "8b8cf34b9b250ab9d62673268e8ab56a",
+    "url": "/resume/static/media/contact.8b8cf34b.svg"
+  },
+  {
+    "revision": "193036c6130dcbb9bdcb6269e0c8658d",
+    "url": "/resume/static/media/education.193036c6.svg"
+  },
+  {
+    "revision": "5a11cc1afe624e5ec76487843674f812",
+    "url": "/resume/static/media/email.5a11cc1a.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/resume/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "4944eed5276c78e6393aab0b7ad3b44a",
+    "url": "/resume/static/media/home.4944eed5.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/resume/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "ae5d1706579161f96f34919fe013592b",
+    "url": "/resume/static/media/objective.ae5d1706.svg"
+  },
+  {
+    "revision": "065f673c5e93d0c68e9dde6911291eb6",
+    "url": "/resume/static/media/projects.065f673c.svg"
+  },
+  {
+    "revision": "8383df7d0bcacbe3dd80806f7fa864c9",
+    "url": "/resume/static/media/skills.8383df7d.svg"
+  },
+  {
+    "revision": "b855d7ffff7240d3236fb73d3bf92ec1",
+    "url": "/resume/static/media/work-experience.b855d7ff.svg"
+  }
+]);
